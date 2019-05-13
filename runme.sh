b5054b866a87a69a1542165e6345ace2f1ad8582
qiuqiangkong <qiuqiangkong@gmail.com>
2019-05-14 00:43:52 +0100

    update runme.sh

--- runme.sh
+++ runme.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # You need to modify this path to your downloaded dataset directory
-DATASET_DIR='/vol/vssp/cvpnobackup/scratch_4weeks/qk00006/dcase2019/task1/dataset_root'
+DATASET_DIR='/vol/vssp/datasets/audio/dcase2019/task1/dataset_root'
 
 # You need to modify this path to your workspace to store features and models
 WORKSPACE='/vol/vssp/msos/qk/workspaces/dcase2019_task1'
@@ -10,6 +10,7 @@ GPU_ID=1
 MODEL_TYPE='Cnn_9layers_AvgPooling'
 BATCH_SIZE=32
 
+############ Train and validate on development dataset ############
 # Calculate feature
 python utils/features.py calculate_feature_for_all_audio_files --dataset_dir=$DATASET_DIR --subtask='a' --data_type='development' --workspace=$WORKSPACE
 python utils/features.py calculate_feature_for_all_audio_files --dataset_dir=$DATASET_DIR --subtask='b' --data_type='development' --workspace=$WORKSPACE
@@ -20,7 +21,6 @@ python utils/features.py calculate_scalar --subtask='a' --data_type='development
 python utils/features.py calculate_scalar --subtask='b' --data_type='development' --workspace=$WORKSPACE
 python utils/features.py calculate_scalar --subtask='c' --data_type='development' --workspace=$WORKSPACE
 
-############ Train and validate on development dataset ############
 # Subtask A
 CUDA_VISIBLE_DEVICES=$GPU_ID python pytorch/main.py train --dataset_dir=$DATASET_DIR --workspace=$WORKSPACE --subtask='a' --data_type='development' --holdout_fold=1 --model_type=$MODEL_TYPE --batch_size=$BATCH_SIZE --cuda
 
@@ -39,17 +39,23 @@ CUDA_VISIBLE_DEVICES=$GPU_ID python pytorch/main.py inference_validation --datas
 # Plot statistics
 python utils/plot_results.py --workspace=$WORKSPACE --subtask=a
 
+############ Train on full data without validation, inference on leaderboard and evaluation data ############
 
-############ Train on full development dataset without validation ############
-# Subtask A
+# Extract features for leaderboard data
+python utils/features.py calculate_feature_for_all_audio_files --dataset_dir=$DATASET_DIR --subtask='a' --data_type='leaderboard' --workspace=$WORKSPACE
+python utils/features.py calculate_feature_for_all_audio_files --dataset_dir=$DATASET_DIR --subtask='b' --data_type='leaderboard' --workspace=$WORKSPACE
+python utils/features.py calculate_feature_for_all_audio_files --dataset_dir=$DATASET_DIR --subtask='c' --data_type='leaderboard' --workspace=$WORKSPACE
+
+# Train on full data
 CUDA_VISIBLE_DEVICES=$GPU_ID python pytorch/main.py train --dataset_dir=$DATASET_DIR --workspace=$WORKSPACE --subtask='a' --data_type='development' --holdout_fold='none' --model_type=$MODEL_TYPE --batch_size=$BATCH_SIZE --cuda
 
-# Subtask B
 CUDA_VISIBLE_DEVICES=$GPU_ID python pytorch/main.py train --dataset_dir=$DATASET_DIR --workspace=$WORKSPACE --subtask='b' --data_type='development' --holdout_fold='none' --model_type=$MODEL_TYPE --batch_size=$BATCH_SIZE --cuda
 
-# Subtask C
 CUDA_VISIBLE_DEVICES=$GPU_ID python pytorch/main.py train --dataset_dir=$DATASET_DIR --workspace=$WORKSPACE --subtask='c' --data_type='development' --holdout_fold='none' --model_type=$MODEL_TYPE --batch_size=$BATCH_SIZE --cuda
 
+# Inference on leaderboard data
+CUDA_VISIBLE_DEVICES=$GPU_ID python pytorch/main.py inference_evaluation --workspace=$WORKSPACE --subtask='a' --data_type='leaderboard' --model_type=$MODEL_TYPE --iteration=5000 --batch_size=$BATCH_SIZE --cuda
 
-############ END ############
+CUDA_VISIBLE_DEVICES=$GPU_ID python pytorch/main.py inference_evaluation --workspace=$WORKSPACE --subtask='b' --data_type='leaderboard' --model_type=$MODEL_TYPE --iteration=5000 --batch_size=$BATCH_SIZE --cuda
 
+CUDA_VISIBLE_DEVICES=$GPU_ID python pytorch/main.py inference_evaluation --workspace=$WORKSPACE --subtask='c' --data_type='leaderboard' --model_type=$MODEL_TYPE --iteration=5000 --batch_size=$BATCH_SIZE --cuda
