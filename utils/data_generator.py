e64f35f7674ab37c1a2fd48fd65a61eb25f552c4
qiuqiangkong <qiuqiangkong@gmail.com>
2019-05-14 00:44:13 +0100

    add Base and EvaluationGenerator

--- utils/data_generator.py
+++ utils/data_generator.py
@@ -12,7 +12,52 @@ from utilities import scale, read_metadata, sparse_to_categorical
 import config
 
 
-class DataGenerator(object):
+class Base(object):
+    
+    def __init__(self):
+        '''Base class for data generator
+        '''
+        pass
+
+    def load_hdf5(self, hdf5_path):
+        '''Load hdf5 file. 
+        
+        Returns:
+          data_dict: dict of data, e.g.:
+            {'audio_name': np.array(['a.wav', 'b.wav', ...]), 
+             'feature': (audios_num, frames_num, mel_bins)
+             'target': (audios_num,), 
+             ...}
+        '''
+        data_dict = {}
+        
+        with h5py.File(hdf5_path, 'r') as hf:
+            data_dict['audio_name'] = np.array(
+                [audio_name.decode() for audio_name in hf['audio_name'][:]])
+
+            data_dict['feature'] = hf['feature'][:].astype(np.float32)
+            
+            if 'scene_label' in hf.keys():
+                data_dict['target'] = np.array(
+                    [self.lb_to_idx[scene_label.decode()] \
+                        for scene_label in hf['scene_label'][:]])
+                
+            if 'identifier' in hf.keys():
+                data_dict['identifier'] = np.array(
+                    [identifier.decode() for identifier in hf['identifier'][:]])
+                
+            if 'source_label' in hf.keys():
+                data_dict['source_label'] = np.array(
+                    [source_label.decode() \
+                        for source_label in hf['source_label'][:]])
+            
+        return data_dict
+
+    def transform(self, x):
+        return scale(x, self.scalar['mean'], self.scalar['std'])
+
+
+class DataGenerator(Base):
     
     def __init__(self, feature_hdf5_path, train_csv, validate_csv, holdout_fold, 
         scalar, batch_size, seed=1234):
@@ -48,10 +93,10 @@ class DataGenerator(object):
         validate_meta = read_metadata(validate_csv)
 
         self.train_audio_indexes = self.get_audio_indexes(
-            train_meta, self.data_dict, 'train')
+            train_meta, self.data_dict, holdout_fold, 'train')
             
         self.validate_audio_indexes = self.get_audio_indexes(
-            validate_meta, self.data_dict, 'validate')
+            validate_meta, self.data_dict, holdout_fold, 'validate')
             
         if holdout_fold == 'none':
             self.train_audio_indexes = np.concatenate(
@@ -66,41 +111,7 @@ class DataGenerator(object):
         self.random_state.shuffle(self.train_audio_indexes)
         self.pointer = 0
         
-    def load_hdf5(self, hdf5_path):
-        '''Load hdf5 file. 
-        
-        Returns:
-          data_dict: dict of data, e.g.:
-            {'audio_name': np.array(['a.wav', 'b.wav', ...]), 
-             'feature': (audios_num, frames_num, mel_bins)
-             'target': (audios_num,), 
-             ...}
-        '''
-        data_dict = {}
-        
-        with h5py.File(hdf5_path, 'r') as hf:
-            data_dict['audio_name'] = np.array(
-                [audio_name.decode() for audio_name in hf['audio_name'][:]])
-
-            data_dict['feature'] = hf['feature'][:].astype(np.float32)
-            
-            if 'scene_label' in hf.keys():
-                data_dict['target'] = np.array(
-                    [self.lb_to_idx[scene_label.decode()] \
-                        for scene_label in hf['scene_label'][:]])
-                
-            if 'identifier' in hf.keys():
-                data_dict['identifier'] = np.array(
-                    [identifier.decode() for identifier in hf['identifier'][:]])
-                
-            if 'source_label' in hf.keys():
-                data_dict['source_label'] = np.array(
-                    [source_label.decode() \
-                        for source_label in hf['source_label'][:]])
-            
-        return data_dict
-        
-    def get_audio_indexes(self, meta_data, data_dict, data_type):
+    def get_audio_indexes(self, meta_data, data_dict, holdout_fold, data_type):
         '''Get train or validate indexes. 
         '''
         audio_indexes = []
@@ -111,10 +122,14 @@ class DataGenerator(object):
             if len(loct) > 0:
                 index = loct[0, 0]
                 label = self.idx_to_lb[self.data_dict['target'][index]]
-                if data_type == 'train' and label != 'unknown':
-                    audio_indexes.append(index)
-                elif data_type == 'validate':
-                    audio_indexes.append(index)
+                if holdout_fold != 'none':
+                    if data_type == 'train' and label != 'unknown':
+                        audio_indexes.append(index)
+                    elif data_type == 'validate':
+                        audio_indexes.append(index)
+                else:
+                    if label != 'unknown':
+                        audio_indexes.append(index)
             
         return np.array(audio_indexes)
         
@@ -213,6 +228,63 @@ class DataGenerator(object):
             batch_data_dict['target'] = sparse_to_categorical(
                 sparse_target, self.all_classes_num)
 
+            yield batch_data_dict
+            
+
+class EvaluationDataGenerator(Base):
+    def __init__(self, feature_hdf5_path, scalar, batch_size, seed=1234):
+        '''Data generator for training and validation. 
+        
+        Args:
+          feature_hdf5_path: string, path of hdf5 feature file
+          scalar: object, containing mean and std value
+          batch_size: int
+          seed: int, random seed
+        '''
+        self.scalar = scalar
+        self.batch_size = batch_size
+
+        self.data_dict = self.load_hdf5(feature_hdf5_path)
+
+    def generate_evaluation(self, data_type, max_iteration=None):
+        '''Generate mini-batch data for training. 
+        
+        Args:
+          data_type: 'train' | 'validate'
+          max_iteration: int, maximum iteration to validate to speed up validation
+        
+        Returns:
+          batch_data_dict: dict containing audio_name, feature and target
+        '''
+        
+        batch_size = self.batch_size 
+        audio_indexes = np.arange(len(self.data_dict['audio_name']))
+
+        iteration = 0
+        pointer = 0
+        
+        while True:
+            if iteration == max_iteration:
+                break
+
+            # Reset pointer
+            if pointer >= len(audio_indexes):
+                break
+
+            # Get batch audio_indexes
+            batch_audio_indexes = audio_indexes[pointer: pointer + batch_size]                
+            pointer += batch_size
+            iteration += 1
+
+            batch_data_dict = {}
+
+            batch_data_dict['audio_name'] = \
+                self.data_dict['audio_name'][batch_audio_indexes]
+            
+            batch_feature = self.data_dict['feature'][batch_audio_indexes]
+            batch_feature = self.transform(batch_feature)
+            batch_data_dict['feature'] = batch_feature
+            
             yield batch_data_dict
             
     def transform(self, x):
